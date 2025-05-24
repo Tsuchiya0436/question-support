115f68ede03f6712f6a8a40ec773f7837b51c890
Shogo <jt58ypmy@gmail.com>
2025-05-24 22:55:52 +0900

    feat: ログイン機能の実装

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,18 +1,25 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { AuthProvider } from "./AuthContext";
+
 import Form from "./components/Form";
+import LoginPage from "./components/LoginPage";
 import AdminDashboard from "./components/AdminDashboard";
 import AdminQuestionDetail from "./components/AdminQuestionDetail";
 
-function App() {
+export default function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Form />} />
-        <Route path="/admin/dashboard" element={<AdminDashboard />} />
-        <Route path="/admin/dashboard/detail/:id" element={<AdminQuestionDetail />} />
-      </Routes>
-    </Router>
+    <AuthProvider>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Form />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/admin/dashboard" element={<AdminDashboard />} />
+          <Route
+            path="/admin/dashboard/detail/:id"
+            element={<AdminQuestionDetail />}
+          />
+        </Routes>
+      </Router>
+    </AuthProvider>
   );
 }
-
-export default App;
